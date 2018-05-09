d2329d704b73e2b5016aa0b124b1f036116de0f9
sri <susanna.riccardi@usi.ch>
2018-05-09 11:18:08 +0200

    network

--- sat-colors/src/App.js
+++ sat-colors/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Banner from './components/Banner';
+import GraphContainer from './components/GraphContainer';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
         return (
             <div className="App">
                 <Banner />
+                <GraphContainer />
             </div>
         );
     }
