7a8764f49cfdfc78e015beee89b8bec58197f901
Marco <marco.mt19@gmail.com>
2018-05-16 15:05:30 +0200

    added server solver

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var formidable = require('formidable');
-var fs = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const { exec } = require('child_process');
+
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -10,6 +12,25 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
+app.post('/problem', function(req, res){
+  // fs.writeFileSync('solver/graph.json', JSON.stringify(req.body));
+
+  const child = exec('cd solver && python mapcolor.py graph.json', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+
+    // console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+
+    const solution = JSON.parse(fs.readFileSync('solver/solution.json', 'utf8'));
+
+    res.send(solution);
+  });
+
+
+});
 
 
 var server = app.listen(3000, function(){
